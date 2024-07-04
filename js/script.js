0a389790aff971544874757513416d123c0cb49d
Julian Yuki Morhard <111897050+YukiOnTour@users.noreply.github.com>
2024-07-04 17:12:21 +0200

    Update scripts.js to Fetch Data

--- js/script.js
+++ js/script.js
@@ -1,13 +1,27 @@
-// Private variable to hold the pokemonList array!!
 const pokemonRepository = (function() {
-  let pokemonList = [
-    { name: 'Bulbasaur', type: ['Grass', 'Poison'], height: 7 },
-    { name: 'Charmander', type: ['Fire'], height: 6 },
-    { name: 'Squirtle', type: ['Water'], height: 5 },
-    { name: 'Pikachu', type: ['Electric'], height: 4 },
-    { name: 'Snorlax', type: ['Normal'], height: 21 },
-    // Add more Pokémon objects here as needed
-  ];
+  let pokemonList = [];
+
+  function add(pokemon) {
+    if (typeof pokemon === 'object' && pokemon !== null) {
+      const expectedKeys = ['name', 'detailsUrl'];
+      const keys = Object.keys(pokemon);
+      const isValidPokemon = expectedKeys.every(key => keys.includes(key));
+      if (isValidPokemon) {
+        pokemonList.push(pokemon);
+        return true;
+      } else {
+        console.error('Invalid Pokémon object. Please provide an object with the following keys: name, detailsUrl');
+        return false;
+      }
+    } else {
+      console.error('Invalid argument. Please provide a valid Pokémon object.');
+      return false;
+    }
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
 
   function addListItem(pokemon) {
     const pokemonListElement = document.querySelector('.pokemon-list');
@@ -22,41 +36,52 @@ const pokemonRepository = (function() {
     pokemonListElement.appendChild(listItem);
   }
 
+  function loadList() {
+    return fetch('https://pokeapi.co/api/v2/pokemon/?limit=150')
+      .then(response => response.json())
+      .then(json => {
+        json.results.forEach(item => {
+          let pokemon = {
+            name: item.name,
+            detailsUrl: item.url
+          };
+          add(pokemon);
+        });
+      })
+      .catch(e => console.error(e));
+  }
+
+  function loadDetails(pokemon) {
+    return fetch(pokemon.detailsUrl)
+      .then(response => response.json())
+      .then(details => {
+        pokemon.imageUrl = details.sprites.front_default;
+        pokemon.height = details.height;
+        pokemon.types = details.types.map(typeInfo => typeInfo.type.name);
+      })
+      .catch(e => console.error(e));
+  }
+
   function showDetails(pokemon) {
-    console.log(pokemon);
+    loadDetails(pokemon).then(() => {
+      console.log(pokemon);
+    });
   }
 
   return {
-    getAll: function() {
-      return pokemonList;
-    },
-    add: function(pokemon) {
-      if (typeof pokemon === 'object' && pokemon !== null) {
-        const expectedKeys = ['name', 'type', 'height'];
-        const keys = Object.keys(pokemon);
-        const isValidPokemon = expectedKeys.every(key => keys.includes(key));
-        if (isValidPokemon) {
-          pokemonList.push(pokemon);
-          return true;
-        } else {
-          console.error('Invalid Pokémon object. Please provide an object with the following keys: name, type, height');
-          return false;
-        }
-      } else {
-        console.error('Invalid argument. Please provide a valid Pokémon object.');
-        return false;
-      }
-    },
-    findByName: function(name) {
-      return pokemonList.find(pokemon => pokemon.name.toLowerCase() === name.toLowerCase());
-    },
+    getAll: getAll,
+    add: add,
+    loadList: loadList,
+    loadDetails: loadDetails,
     addListItem: addListItem
   };
 })();
 
 function displayPokemonList() {
-  pokemonRepository.getAll().forEach(pokemon => {
-    pokemonRepository.addListItem(pokemon);
+  pokemonRepository.loadList().then(function() {
+    pokemonRepository.getAll().forEach(pokemon => {
+      pokemonRepository.addListItem(pokemon);
+    });
   });
 }
 
